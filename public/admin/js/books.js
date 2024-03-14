e36c7d3604611a70fd7e563e7a747db4b8e02cb4
JuanR4140 <N/A>
2024-03-14 13:20:18 -0500

    Can now add books! Snack also higher z index

--- public/admin/js/books.js
+++ public/admin/js/books.js
@@ -5,6 +5,52 @@ socket.emit("admin-ping", {
   token: getCookie("token")
 });
 
+document.querySelector("#add-book-screen-btn").addEventListener("click", () => {
+  document.querySelector("#add-book-screen").classList.remove("hidden");
+  document.querySelector("#add-book-screen").classList.add("block");
+
+  document.querySelector("#main").classList.remove("block");
+  document.querySelector("#main").classList.add("hidden");
+});
+
+document.querySelector("#add-book-back-btn").addEventListener("click", () => {
+  document.querySelector("#main").classList.remove("hidden");
+  document.querySelector("#main").classList.add("block");
+
+  document.querySelector("#add-book-screen").classList.remove("block");
+  document.querySelector("#add-book-screen").classList.add("hidden");
+});
+
+document.querySelector("#add-book-btn-final").addEventListener("click", () => {
+
+  const image = document.querySelector("#add-book-img");
+  console.log(image.width);
+  console.log(image.height);
+  image.setAttribute("crossorigin", "anonymous");
+
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = image.width;
+  canvas.height = image.height;
+  ctx.drawImage(image, 0, 0, image.width, image.height);
+  const bytes = canvas.toDataURL("image/png").replace(/^data:image\/(png|jpg);base64,/, "");
+
+  socket.emit("admin-add-book", {
+    username: getCookie("username"),
+    token: getCookie("token"),
+
+    isbn: document.querySelector("#add-book-title").getAttribute("isbn"),
+    title: document.querySelector("#add-book-title").innerText,
+    author: document.querySelector("#add-book-author").innerText,
+    genre: document.querySelector("#dropdown-genre-button").innerText,
+    cover: bytes
+  });
+});
+
+socket.on("admin-add-book-result", (data) => {
+  createSnackbar(data.msg, data.bgColor, data.txColor);
+});
+
 socket.on("fatal", () => {
   window.location.href = "/";
 });
--- sockets/admin/admin-add-book.js
+++ sockets/admin/admin-add-book.js
@@ -5,12 +5,15 @@ const { verify } = require("../../utils/verify.js");
 
 module.exports = (socket, users, books, bucket) => {
     socket.on("admin-add-book", async (data) => {
-        console.log("Connection received!");
         let { verified, userRef, user } = await verify(users, data);
         if(!verified) {socket.emit("fatal"); return; }
 
+        data.genre = data.genre.toLowerCase();
+
         if(data.username != process.env.valid_admin_email.split("@")[0]){ socket.emit("fatal"); return; }
         let allowed_genres = ["fiction"];
+
+        if(!data.isbn || !data.title || !data.author || !data.genre){ socket.emit("admin-add-book-result", {msg: "Invalid data.", bgColor: "#FF5555", txColor: "#FFFFFF"}); return; }
 
         if(data.isbn.length == 0 || data.title.length == 0 || data.author.length == 0 || !allowed_genres.includes(data.genre)){
             socket.emit("admin-add-book-result", {msg: "Invalid data.", bgColor: "#FF5555", txColor: "#FFFFFF"});
@@ -26,7 +29,8 @@ module.exports = (socket, users, books, bucket) => {
         const buffer = Buffer.from(data.cover, "base64");
         jimp.read(buffer, async (err, res) => {
             if(err) throw new Error(err);
-            res.quality(50).write("./sockets/admin/media/" + data.isbn + ".jpg");
+            // res.quality(50).write("./sockets/admin/media/" + data.isbn + ".jpg");
+            await res.quality(50).writeAsync("./sockets/admin/media/" + data.isbn + ".jpg");
             let file = "./sockets/admin/media/" + data.isbn + ".jpg";
             await bucket.upload(file);
             fs.unlink("./sockets/admin/media/" + data.isbn + ".jpg", (err) => {});
